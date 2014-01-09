28700bcb7288a6d0b6791905a020542672e2f937
Ad Taylor <ad@adtaylor.co.uk>
2014-01-09 12:39:34 +0000

    Moves local mosaic path to port 3001

--- lib/mosaic_consumer.rb
+++ lib/mosaic_consumer.rb
@@ -1,9 +1,8 @@
 require 'mosaic_consumer/mosaic_consumer_controller'
 module MosaicConsumer
   class Engine < Rails::Engine
-    
     case ENV["RAILS_ENV"].to_sym
-    when :development then config.mosaic_api_url = "http://localhost:3000"
+    when :development then config.mosaic_api_url = "http://localhost:3001"
     else config.mosaic_api_url = "http://mosaic.thebeansgroup.com"
     end
   end
